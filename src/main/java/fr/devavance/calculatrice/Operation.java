ffa22b56ca3b536c81f5d2bd2831fdbbd463540f
SCAnas05 <haanas2020@gmail.com>
2025-10-21 11:51:36 +0200

    Ajout de la classe Operation

--- src/main/java/fr/devavance/calculatrice/Operation.java
+++ src/main/java/fr/devavance/calculatrice/Operation.java
@@ -8,6 +8,28 @@ package fr.devavance.calculatrice;
  *
  * @author kauth_6
  */
-class Operation {
+public class Operation {
+    private String operator;
+    private Integer operande1;
+    private Integer operande2;
+    private Double result;
+    
+    public Operation(String operator, Integer operande1, Integer operande2, Double result){
+        this.operator = operator;
+        this.operande1 = operande1;
+        this.operande2 = operande2;
+        this.result = result;
+    }
+    
+    public Operation() {
+        this("", 0,0, 0.0);
+    }
+    
+    
+    public String getOperator() { return this.operator; }
+    public Integer getOperande1() { return this.operande1; }
+    public Integer getOperande2() { return this.operande2; }
+    public Double getResult() { return this.result; } 
+    
     
 }
